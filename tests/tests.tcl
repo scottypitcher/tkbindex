bf40b574f06546645e9108e0296f79a428c2a4fe
Scott Pitcher <scottypitcher@gmail.com>
2021-06-05 08:16:36 +1000

    SVP 5JUN2021: Added licence

--- tests/tests.tcl
+++ tests/tests.tcl
@@ -1,6 +1,18 @@
 #!/usr/bin/tclsh
 #
-# Copyright (C) Scott Pitcher - 2020.
+# Copyright (C) Scott Pitcher - 2020-2021
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License version 3
+#    as published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You can find a copy of the GNU Lesser General Public License at:
+#    <https://www.gnu.org/licenses/>.
 #
 # A test implementation of bindex in tcl.
 #
